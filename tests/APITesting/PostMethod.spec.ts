8939fdad936366d687b0f59ba45d74657c3c20b8
ShreyB <shreypractice.github@gmail.com>
2026-05-25 23:37:44 +0530

    Updates_2505_23:38

--- tests/APITesting/PostMethod.spec.ts
+++ tests/APITesting/PostMethod.spec.ts
@@ -1,28 +1,51 @@
-import { test, expect} from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("POST Request Using Static Body", async ({ request }) => {
 
     //requestBody Formation
     const requestBody =
     {
-        "firstname": "Jim",
-        "lastname": "Brown",
-        "totalprice": 111,
-        "depositpaid": true,
-        "bookingdates": {
-            "checkin": "2018-01-01",
-            "checkout": "2019-01-01"
+        firstname: "Jim",
+        lastname: "Brown",
+        totalprice: 111,
+        depositpaid: true,
+        bookingdates: {
+            checkin: "2018-01-01",
+            checkout: "2019-01-01"
         },
-        "additionalneeds": "Breakfast"
+        additionalneeds: "Breakfast"
     }
 
     //Sending Post Request
-    const response = await request.post("https://restful-booker.herokuapp.com/booking",{data:requestBody});
-    const responseBody=await response.json();
+    const response = await request.post("/booking", { data: requestBody });
+    const responseBody = await response.json();
     console.log(responseBody);
 
     expect(response.ok()).toBeTruthy();
     expect(response.status()).toBe(200);
 
     expect(responseBody).toHaveProperty("bookingid");
+    expect(responseBody).toHaveProperty("booking");
+    expect(responseBody).toHaveProperty("booking.firstname");
+
+
+    const booking = responseBody.booking;
+
+    expect(booking).toMatchObject(
+        {
+            firstname: "Jim",
+            lastname: "Brown",
+            totalprice: 111,
+            depositpaid: true,
+            additionalneeds: "Breakfast"
+        }
+    )
+
+    expect(booking.bookingdates).toMatchObject(
+        {
+            checkin: "2018-01-01",
+            checkout: "2019-01-01"
+        }
+    )
+
 })
